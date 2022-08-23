01a50a529ca1078100249cca4f0447f83cd4d4d4
Judith <judith.barrajon@gmail.com>
2022-08-23 17:46:46 +0200

    test Region mockito

--- src/test/java/com/group3/wineshop/RegionControllerTest.java
+++ src/test/java/com/group3/wineshop/RegionControllerTest.java
@@ -2,13 +2,23 @@ package com.group3.wineshop;
 
 
 import com.group3.wineshop.Controllers.RegionController;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import static java.nio.file.Paths.get;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static sun.nio.cs.Surrogate.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -20,6 +30,19 @@ public class RegionControllerTest {
     void mostrarTodos() throws Exception{
         mockMvc.perform(MockMvcRequestBuilders
                 .get("/api/region")
-                .contentType("application/json"));
+                .contentType("application/json"))
+                .andExpect(status().isOk()
+                );
     }
+
+    @Test
+    void mostrarRegionId() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/region/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id",Matchers.is(1)))
+                .andExpect(jsonPath("$.name", Matchers.is("Toro"))
+        );
+    }
+
 }
