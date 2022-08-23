da381d04b3f34ab341881cd11f3a976008f17012
Judith <judith.barrajon@gmail.com>
2022-08-23 12:50:51 +0200

    añadida region

---
+++ src/main/java/com/group3/wineshop/entities/Region.java
@@ -0,0 +1,48 @@
+package com.group3.wineshop.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "region")
+public class Region {
+
+    @Id
+    private int id;
+    private String name;
+    private String country;
+
+    public Region(int id, String name, String country) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+    }
+
+    public Region() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
