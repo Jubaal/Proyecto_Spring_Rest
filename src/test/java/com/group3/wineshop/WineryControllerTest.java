f0dc2c6aa1b41e04c05929bded12e4bc8c04bf21
ilba-gft <ilba@gft.com>
2022-08-24 09:04:37 +0200

    winery controller test

---
+++ src/test/java/com/group3/wineshop/WineryControllerTest.java
@@ -0,0 +1,27 @@
+package com.group3.wineshop;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class WineryControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void all() throws Exception {
+		mockMvc.perform(get("/api/winery")
+				.contentType("application/json")
+				).andExpect(status().isOk());
+	}
+
+}
